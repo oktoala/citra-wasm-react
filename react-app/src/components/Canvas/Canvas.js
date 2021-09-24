d5cee358314f9d2dbe6c297a3df62799ceeafcbf
oktoala <okta.yoga.syah@gmail.com>
2021-09-24 09:11:37 +0800

    Move to hooks and functional

--- react-app/src/components/Canvas/Canvas.js
+++ react-app/src/components/Canvas/Canvas.js
@@ -1,80 +1,203 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import img_src from './daisies.jpg';
 
-class Canvas extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0,
-      loadedWasm: false,
-      isLoaded: false,
-      wasm: null,
-      img: null
-    };
-  }
-  componentDidMount() {
-    this.loadWasm();
-  }
 
-  drawOriginalImage = async () => {
+// class Canvas extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       count: 0,
+//       loadedWasm: false,
+//       isLoaded: false,
+//       wasm: null,
+//       img: null
+//     };
+//   }
+//   componentDidMount() {
+//     this.loadWasm();
+//   }
+
+//   drawOriginalImage = async () => {
+//     const img = new Image();
+
+//     img.onload = () => {
+//       this.img = img;
+//       const canvas = this.refs.canvas;
+//       canvas.width = img.width;
+//       canvas.height = img.height;
+//       const ctx = canvas.getContext("2d");
+
+//       ctx.drawImage(img, 0, 0);
+
+//     }
+//     img.src = img_src;
+//   }
+
+//   loadWasm = async () => {
+
+//     try {
+//       const photon = await import('@silvia-odwyer/photon');
+
+//       this.wasm = photon;
+
+//       this.drawOriginalImage();
+
+//     } finally {
+//       console.log("loaded wasm successfully");
+//       this.loadedWasm = true;
+//       console.log("this.loadedWasm is", this.loadedWasm);
+//     }
+
+//   }
+
+//   alterChannel = async (channel_index) => {
+//     const canvas1 = this.refs.canvas;
+//     const ctx = canvas1.getContext("2d");
+
+//     ctx.drawImage(this.img, 0, 0);
+
+//     let photon = this.wasm;
+
+//     // Convert the canvas and context to a PhotonImage
+//     let image = photon.open_image(canvas1, ctx);
+
+//     // Filter the image
+//     photon.alter_channel(image, channel_index, 50);
+
+//     // Replace the current canvas' ImageData with the new image's ImageData.
+//     photon.putImageData(canvas1, ctx, image);
+
+//   }
+
+//   effectPipeline = async () => {
+//     const canvas1 = this.refs.canvas;
+//     const ctx = canvas1.getContext("2d");
+
+//     ctx.drawImage(this.img, 0, 0);
+
+//     let photon = this.wasm;
+//     let phtimg = photon.open_image(canvas1, ctx);
+
+//     console.time("PHOTON_WITH_RAWPIX");
+//     photon.alter_channel(phtimg, 2, 70);
+//     photon.grayscale(phtimg);
+//     console.timeEnd("PHOTON_WITH_RAWPIX");
+
+//     // // Replace the current canvas' ImageData with the new image's ImageData.
+//     photon.putImageData(canvas1, ctx, phtimg);
+
+
+
+//     console.time("PHOTON_CONSTR");
+//     // photon.canvas_wasm_only(canvas1, ctx);
+//     console.timeEnd("PHOTON_CONSTR");
+//   }
+
+//   render() {
+//     return (
+//       <div className="default">
+
+//         <div className="sidebar">
+//           <h3 className="logo">Photon</h3>
+
+//           <ul>
+//             <h4>Channels</h4>
+//             <li id="alter_red" onClick={() => this.alterChannel(0)}>Increase Red Channel</li>
+//             <li id="alter_green" onClick={() => this.alterChannel(1)}>Increase Green Channel</li>
+//             <li id="alter_blue" onClick={() => this.alterChannel(2)}>Increase Blue Channel</li>
+
+//             <li id="alter_blue" onClick={this.effectPipeline}>Inc Channel + Threshold</li>
+
+//           </ul>
+//         </div>
+
+
+//         <div className="main">
+//           <div className="main_content">
+
+//             <section className="content">
+//               <h2>Image</h2>
+//               <canvas ref="canvas" />
+//             </section>
+
+//             <section className="benchmarks">
+//               <div id="time"></div>
+//               <div id="code"></div>
+//             </section>
+
+//           </div>
+
+//         </div>
+
+//       </div>
+//     )
+//   }
+// }
+
+const Canvas = () => {
+  const [loadedWasm, setLoadedWasm] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [wasm, setWasm] = useState(null);
+  const [img, setImg] = useState(null);
+
+  const canvasRef = useRef(null);
+  useEffect(() => {
+    loadWasm();
+
+  }, [])
+
+  async function drawOriginalImage() {
     const img = new Image();
 
     img.onload = () => {
-      this.img = img;
-      const canvas = this.refs.canvas;
+      setImg(img);
+      const canvas = canvasRef.current;
       canvas.width = img.width;
       canvas.height = img.height;
       const ctx = canvas.getContext("2d");
 
       ctx.drawImage(img, 0, 0);
-
     }
+
     img.src = img_src;
   }
 
-  loadWasm = async () => {
-
+  async function loadWasm() {
     try {
       const photon = await import('@silvia-odwyer/photon');
 
-      this.wasm = photon;
+      setWasm(photon);
 
-      this.drawOriginalImage();
-
+      drawOriginalImage();
     } finally {
-      console.log("loaded wasm successfully");
-      this.loadedWasm = true;
-      console.log("this.loadedWasm is", this.loadedWasm);
+      console.log("Loaded Wasm Successfully");
+      setLoadedWasm(true);
+      console.log(`loadedWasm is ${loadedWasm}`);
     }
-
   }
 
-  alterChannel = async (channel_index) => {
-    const canvas1 = this.refs.canvas;
+  async function alterChannel(channel_index) {
+    const canvas1 = canvasRef.current;
     const ctx = canvas1.getContext("2d");
 
-    ctx.drawImage(this.img, 0, 0);
+    ctx.drawImage(img, 0, 0);
 
-    let photon = this.wasm;
+    let photon = wasm;
 
-    // Convert the canvas and context to a PhotonImage
     let image = photon.open_image(canvas1, ctx);
 
-    // Filter the image
     photon.alter_channel(image, channel_index, 50);
 
-    // Replace the current canvas' ImageData with the new image's ImageData.
     photon.putImageData(canvas1, ctx, image);
-
   }
 
-  effectPipeline = async () => {
-    const canvas1 = this.refs.canvas;
+  async function effectPipeline() {
+    const canvas1 = canvasRef.current;
     const ctx = canvas1.getContext("2d");
 
-    ctx.drawImage(this.img, 0, 0);
+    ctx.drawImage(img, 0, 0);
 
-    let photon = this.wasm;
+    let photon = wasm;
     let phtimg = photon.open_image(canvas1, ctx);
 
     console.time("PHOTON_WITH_RAWPIX");
@@ -82,55 +205,52 @@ class Canvas extends React.Component {
     photon.grayscale(phtimg);
     console.timeEnd("PHOTON_WITH_RAWPIX");
 
-    // // Replace the current canvas' ImageData with the new image's ImageData.
     photon.putImageData(canvas1, ctx, phtimg);
-
-
 
     console.time("PHOTON_CONSTR");
     // photon.canvas_wasm_only(canvas1, ctx);
     console.timeEnd("PHOTON_CONSTR");
   }
 
-  render() {
-    return (
-      <div className="default">
 
-        <div className="sidebar">
-          <h3 className="logo">Photon</h3>
+  return (
+    <div className="default">
 
-          <ul>
-            <h4>Channels</h4>
-            <li id="alter_red" onClick={() => this.alterChannel(0)}>Increase Red Channel</li>
-            <li id="alter_green" onClick={() => this.alterChannel(1)}>Increase Green Channel</li>
-            <li id="alter_blue" onClick={() => this.alterChannel(2)}>Increase Blue Channel</li>
+      <div className="sidebar">
+        <h3 className="logo">Photon</h3>
 
-            <li id="alter_blue" onClick={this.effectPipeline}>Inc Channel + Threshold</li>
+        <ul>
+          <h4>Channels</h4>
+          <li id="alter_red" onClick={() => alterChannel(0)}>Increase Red Channel</li>
+          <li id="alter_green" onClick={() => alterChannel(1)}>Increase Green Channel</li>
+          <li id="alter_blue" onClick={() => alterChannel(2)}>Increase Blue Channel</li>
 
-          </ul>
-        </div>
+          <li id="alter_blue" onClick={effectPipeline}>Inc Channel + Threshold</li>
+
+        </ul>
+      </div>
 
 
-        <div className="main">
-          <div className="main_content">
+      <div className="main">
+        <div className="main_content">
 
-            <section className="content">
-              <h2>Image</h2>
-              <canvas ref="canvas" />
-            </section>
+          <section className="content">
+            <h2>Image</h2>
+            <canvas ref={canvasRef} />
+          </section>
 
-            <section className="benchmarks">
-              <div id="time"></div>
-              <div id="code"></div>
-            </section>
-
-          </div>
+          <section className="benchmarks">
+            <div id="time"></div>
+            <div id="code"></div>
+          </section>
 
         </div>
 
       </div>
-    )
-  }
+
+    </div>
+  )
 }
+
 
 export default Canvas
