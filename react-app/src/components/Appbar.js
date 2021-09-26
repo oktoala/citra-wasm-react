eb79d659aa2c608169b9c5c1aadd56eb2efb5b17
oktoala <okta.yoga.syah@gmail.com>
2021-09-26 15:19:29 +0800

    Add tooltip

--- react-app/src/components/Appbar.js
+++ react-app/src/components/Appbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Tab from '@mui/material/Tab';
+import Tooltip from '@mui/material/Tooltip';
 import TabList from '@mui/lab/TabList';
 import PaletteIcon from '@mui/icons-material/Palette';
 import FilterIcon from '@mui/icons-material/Filter';
@@ -16,11 +17,20 @@ const Appbar = (props) => {
             ml: { sm: `${drawerWidth}px` },
         }}>
             <TabList onChange={props.onChange}>
-                <Tab label={<PaletteIcon/>} value="0"/>
-                <Tab label={<FilterIcon/>} value="1"/>
-                <Tab label={<EqualizerIcon/>} value="2"/>
+                <TabTip title="Color Space" icon={<PaletteIcon/>} value="0" />
+                <TabTip title="Filter" icon={<FilterIcon/>} value="1" />
+                <TabTip title="Histogram" icon={<EqualizerIcon/>} value="2" />
             </TabList>
         </AppBar>
+    );
+}
+
+const TabTip = (props) => {
+
+    return (
+        <Tooltip title={props.title} value={props.value}>
+            <Tab label={props.icon} value={props.value}/>
+        </Tooltip>
     );
 }
 
